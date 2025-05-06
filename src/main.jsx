db14fa1c2074072d7e57f20a5a97a3e065007fcb
sagaradreddy <sagarad671@gmail.com>
2025-05-06 20:22:49 +0530

    loan calculator

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { ThemeProviderCustom } from './context/ThemeProviderCustom';
+import { BrowserRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+    <ThemeProviderCustom>
+     <App />
+    </ThemeProviderCustom>
+    </BrowserRouter>
+  </StrictMode>
 )
